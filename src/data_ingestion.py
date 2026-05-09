2803aee338de384f7131ecc841079dfe474bf836
Akshay <akshayparate61@gmail.com>
2026-05-09 15:49:52 +0530

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -27,6 +27,24 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+#YAML implimentation
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file"""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpeected error: %s', e)
+        raise
+
 # Data Loading
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -72,7 +90,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 # Running file func sequencially
 def main():
     try:
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
         data_path = 'https://raw.githubusercontent.com/AkshayParate61/Datasets/refs/heads/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
